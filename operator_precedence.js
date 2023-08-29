0466d8ee46da8b1e18e36b4042dd96f84b911079
jeevanism <jeevanism@gmail.com>
2023-08-29 11:08:58 +0100

    Explain  the operator precedence and its effect in result

---
+++ operator_precedence.js
@@ -0,0 +1,26 @@
+/**
+ * The arithmetic operator has different effect depends on the position
+ * Below code demonstrate the effect
+ */
+
+
+/**
+ * a variable named value is declared and assigned the value 5.
+ * The console.log(value); statement prints the current value of the value variable, which is 5, to the console.
+ */
+
+let value = 5;
+console.log(value)
+
+/**
+ * In this line, the console.log(value++); statement prints the current value of the value variable (5) to the console, and then increments the value by 1. This is the post-increment operation.
+ * After the increment operation, the value of value becomes 6.
+ */
+console.log(value++)
+
+/**
+ * In this line, the console.log(++value); statement increments the value of the value variable by 1 first (pre-increment), and then prints the updated value (7) to the console.
+ * After the pre-increment operation, the value of value becomes 7.
+ */
+console.log(++value)
+
