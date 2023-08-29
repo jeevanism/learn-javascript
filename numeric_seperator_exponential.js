a74773d9dea038c1b743bfc268073b7e1eda8330
jeevanism <jeevanism@gmail.com>
2023-08-29 11:28:48 +0100

    Explain the numeric seperator and scientific operators

--- numeric_seperator_exponential.js
+++ numeric_seperator_exponential.js
@@ -7,6 +7,13 @@
 
 
 
+/**
+ * 45_43_000_000: Underscores (_) are used as visual separators in numeric literals for improved readability.
+ * They have no impact on the actual value. So, 45_43_000_000 is the same as 45430000000.
+ * 2e5: This is scientific notation. The e indicates exponent notation, and 2e5 means 2 multiplied by 10 raised to the power of 5, which equals 200000.
+ * Adding 45430000000 and 200000 together gives you 45430200000.
+ */
+
 const result = 45_43_000_000 + 2e5
 console.log(result)
 
