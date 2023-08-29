d185e86a06a1821e8eaf505e484d27e954813ffb
jeevanism <jeevanism@gmail.com>
2023-08-29 11:28:08 +0100

    Explain the numeric seperator and scientific operators

---
+++ numeric_seperator_exponential.js
@@ -0,0 +1,12 @@
+/**
+ * Javascript ES2021 has a numeric seperator '-'
+ * This helps to get more readability for long numbers
+ * The numerica seperator has no effect other than visual readability
+ */
+
+
+
+
+const result = 45_43_000_000 + 2e5
+console.log(result)
+
